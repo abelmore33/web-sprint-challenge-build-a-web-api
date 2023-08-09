1062d3b1a8f2742e904ea10e54b784b42958e7b1
Aaron Belmore <belmoreaaron9@gmail.com>
2023-08-08 20:51:07 -0400

    added proj router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,17 @@
 // Write your "projects" router here!
+const express = require("express");
+const Projects = require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "No data" });
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
+
+const projectsRoutes = require("./projects/projects-router");
+
 const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use("/api/projects", projectsRoutes);
 
 module.exports = server;
