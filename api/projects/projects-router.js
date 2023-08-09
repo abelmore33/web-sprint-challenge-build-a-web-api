3fa9a422ce62f7bbecf3744e52d5acce9b66d1a1
Aaron Belmore <belmoreaaron9@gmail.com>
2023-08-09 10:27:23 -0400

    Completed projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const Projects = require("./projects-model");
 
+const { validateProjId, validateProj } = require("./projects-middleware");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -14,7 +16,7 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateProjId, (req, res) => {
   Projects.get(req.params.id)
     .then((project) => {
       res.status(200).json(project);
@@ -24,6 +26,41 @@ router.get("/:id", (req, res) => {
         message: "Project not found",
       });
     });
+});
+
+router.post("/", validateProj, (req, res) => {
+  const newPost = req.body;
+  Projects.insert(newPost)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((err) =>
+      res.status(500).json({
+        message: "no post found",
+      })
+    );
+});
+
+router.put("/:id", validateProj, (req, res) => {
+  Projects.update(req.params.id, req.body)
+    .then((updatedPost) => {
+      res.status(201).json(updatedPost);
+    })
+    .catch((err) =>
+      res.status(500).json({
+        message: "Error",
+      })
+    );
+});
+
+router.delete("/:id", validateProjId, (req, res) => {
+  Projects.remove(req.params.id)
+    .then((removedPost) => res.status(200).json(removedPost))
+    .catch((err) =>
+      res.status(500).json({
+        message: err.message,
+      })
+    );
 });
 
 module.exports = router;
